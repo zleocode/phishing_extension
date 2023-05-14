0c409b5dc1bc7411cc7636d8c6ddaf954077cd44
Leonardo Faganelli <leonardofaganelli@hotmail.com>
2023-05-14 16:58:50 -0300

    verificando url da aba aberta. separado arquivo com lista de dominios validados.

--- extensao.js
+++ extensao.js
@@ -2,88 +2,51 @@ const form = document.querySelector("#form");
 const campo = document.querySelector("input[name='campo']");
 const resultado = document.querySelector("#resultado");
 
-const listaComparacao = [
-    "https://www.mercadolivre.com.br/",
-    "https://www.americanas.com.br/",
-    "https://www.magazineluiza.com.br/",
-    "https://www.submarino.com.br/",
-    "https://www.shoptime.com.br/",
-    "https://www.amazon.com.br/",
-    "https://www.casasbahia.com.br/",
-    "https://www.extra.com.br/",
-    "https://www.pontofrio.com.br/",
-    "https://www.carrefour.com.br/",
-    "https://www.netshoes.com.br/",
-    "https://www.zattini.com.br/",
-    "https://www.dafiti.com.br/",
-    "https://www.lojasrenner.com.br/",
-    "https://www.riachuelo.com.br/",
-    "https://www.marisa.com.br/",
-    "https://www.cea.com.br/",
-    "https://www.havan.com.br/",
-    "https://www.saraiva.com.br/",
-    "https://www.livrariacultura.com.br/",
-    "https://www.leroymerlin.com.br/",
-    "https://www.magazineluiza.com.br/marketplace/",
-    "https://www.madeiramadeira.com.br/",
-    "https://www.kalunga.com.br/",
-    "https://www.fastshop.com.br/",
-    "https://www.tokstok.com.br/",
-    "https://www.pernambucanas.com.br/",
-    "https://www.lojaskd.com.br/",
-    "https://www.etna.com.br/",
-    "https://www.casasajita.com.br/",
-    "https://www.petz.com.br/",
-    "https://www.petlove.com.br/",
-    "https://www.petco.com.br/",
-    "https://www.zara.com/br/",
-    "https://www.adidas.com.br/",
-    "https://www.nike.com.br/",
-    "https://www.centauro.com.br/",
-    "https://www.decathlon.com.br/",
-    "https://www.apple.com/br/",
-    "https://www.samsung.com.br/",
-    "https://www.kabum.com.br/",
-    "https://www.walmart.com.br/",
-    "https://www.kaufen.com.br/",
-    "https://www.farmrio.com.br/",
-    "https://www.farfetch.com/br/",
-    "https://www.electrolux.com.br/",
-    "https://www.colombo.com.br/",
-    "https://www.angeloni.com.br/",
-    "https://www.buscape.com.br/",
-    "https://www.zoom.com.br/",];
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  var url = tabs[0].url;
+  // console.log(url); // exibe a URL da aba ativa no console
+  campo.value = url;
+});
 
-const listaEncurtada = [
-  "bit.ly",
-  "tinyurl.com",
-  "goo.gl",
-  "ow.ly",
-  "buff.ly",
-  "t.co",
-  "adf.ly",
-  "is.gd",
-  "mcaf.ee",
-  "fur.ly",
-  "tr.im",
-  "ow.ly",
-  "tiny.cc",
-  "v.gd",
-  "cli.gs",
-  "idek.net",
-  "kl.am",
-  "soo.gd",
-  "zz.gd",
-  "yourls.org"
-];
+function verifyCertificate(url) {
+  fetch(url)
+    .then(response => response.arrayBuffer())
+    .then(buffer => {
+      const crypto = window.crypto.subtle;
+      const cert = new Uint8Array(buffer);
+      const signature = new Uint8Array(0); // Assinatura digital do certificado
+      const publicKey = null;
+      crypto.verify(
+        { name: "RSASSA-PKCS1-v1_5" },
+        publicKey,
+        signature,
+        cert
+      ).then(result => {
+        if (result) {
+          console.log("Certificado válido!");
+        } else {
+          console.log("Certificado inválido!");
+        }
+      }).catch(error => {
+        console.error("Erro ao verificar o certificado:", error);
+      });
+    })
+    .catch(error => {
+      console.error("Erro ao buscar o certificado:", error);
+    });
+}
+  
+
 
 form.addEventListener("submit", function(event) {
   event.preventDefault();
+
+  const input = campo.value.replace(/www\./, '');
+  const isEncurtada = listaEncurtada.some(valor => input.includes(valor.replace(/www\./, '')));
+  const isConfiavel = listaComparacao.some(valor => input.includes(valor.replace(/www\./, '')));
   
-  const input = campo.value;
-  const isEncurtada = listaEncurtada.some(valor => input.includes(valor));
-  const isConfiavel = listaComparacao.some(valor => input.includes(valor));
-  
+  // verifyCertificate(input);
+
   if (isEncurtada) {
     resultado.textContent = 'Esta URL é encurtada e não é possível verificar a confiabilidade.';
     resultado.className = 'warning';
